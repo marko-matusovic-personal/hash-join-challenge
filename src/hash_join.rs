15767025e2d30f54e7e0a5758bad0b98970c2b06
Marko Matusovic <matusovic.m@gmail.com>
2022-12-01 10:23:37 +0100

    Fix classic hash

--- src/hash_join.rs
+++ src/hash_join.rs
@@ -1,14 +1,11 @@
-use std::{collections::HashMap, hash::BuildHasherDefault, time::Instant};
+use std::{collections::HashMap, time::Instant};
 
-use blake3;
 use file_handling::{
     input::reader::Reader,
     result::writer::JoinWriter,
     types::{InputRow, JoinRow},
 };
 use rand::Rng;
-
-type HashMapCustom<K, V> = HashMap<K, V, BuildHasherDefault<blake3::Hasher>>;
 
 const VARIANT: &str = "500k";
 
@@ -23,13 +20,14 @@ pub fn run() {
 }
 
 fn run_simple_hash_join() {
+    println!("Started Simple hash join");
+
     let path_r: &str = &format!("data/R-{}.u32f32", VARIANT);
     let path_s: &str = &format!("data/S-{}.u32f32", VARIANT);
 
     let mut reader_r = Reader::open(&path_r);
     let mut reader_s = Reader::open(&path_s);
 
-    // let mut map: HashMapCustom<u32, Vec<InputRow>> = HashMapCustom::default();
     let mut map: HashMap<u32, Vec<InputRow>> = HashMap::new();
 
     let mut r_size: u32 = 0;
@@ -64,60 +62,68 @@ fn run_simple_hash_join() {
 }
 
 fn run_classic_hash_join() {
+    println!("Started classic hash join");
+
     let path_r: &str = &format!("data/R-{}.u32f32", VARIANT);
     let path_s: &str = &format!("data/S-{}.u32f32", VARIANT);
     let path_out: &str = &format!("data/R|x|S-{}.u32f32", VARIANT);
 
-    println!("Started classic hash join");
-
     let mut reader_r = Reader::open(&path_r);
-    let mut reader_s = Reader::open(&path_s);
 
-    // let mut map: HashMapCustom<u32, Vec<InputRow>> = HashMapCustom::default();
     let mut map: HashMap<u32, Vec<InputRow>> = HashMap::new();
 
     let mut writer = JoinWriter::create(path_out);
 
-    let mut block = 0;
-    let mut row_count = 0;
+    let mut block_count = 0;
+    let mut block_sizes: Vec<u32> = Vec::new();
+    let mut hit_count = 0;
 
-    'out: loop {
-        println!("Hashing block {}", block);
-        block += 1;
-
+    loop {
         map.clear();
-
         let mut block_size = 0;
+
         loop {
             if running_low_on_memory() {
-                // out of memory check
-                println!("Block size {} entries", block_size);
                 break;
+            };
+            match reader_r.read() {
+                None => break,
+                Some(InputRow(id, val)) => {
+                    block_size += 1;
+                    map.entry(id).or_insert(Vec::new()).push(InputRow(id, val));
+                }
             }
-            if let Some(InputRow(id, val)) = reader_r.read() {
-                block_size += 1;
-                map.entry(id).or_insert(Vec::new()).push(InputRow(id, val));
-                continue;
-            }
-            break 'out;
         }
+
+        if map.is_empty() {
+            break;
+        };
+
+        block_count += 1;
+        block_sizes.push(block_size);
+
+        let mut reader_s = Reader::open(&path_s);
 
         loop {
-            if let Some(InputRow(id_s, val_s)) = reader_s.read() {
-                if let Some(rows) = map.get(&id_s) {
-                    rows.iter().for_each(|&InputRow(_, val_r)| {
-                        row_count += 1;
-                        writer.write(&JoinRow(id_s, val_s, val_r));
-                    });
+            match reader_s.read() {
+                None => break,
+                Some(InputRow(id_s, val_s)) => {
+                    if let Some(rows) = map.get(&id_s) {
+                        rows.iter().for_each(|&InputRow(_, val_r)| {
+                            hit_count += 1;
+                            writer.write(&JoinRow(id_s, val_s, val_r));
+                        });
+                    }
                 }
-                continue;
             }
-            break;
         }
     }
-    println!("Size of R |x| S: {} entries", row_count);
 
-    println!("Finished");
+    println!(
+        "Number of blocks: {} with sizes: {:?}",
+        block_count, block_sizes
+    );
+    println!("Size of R |x| S: {} entries", hit_count);
 }
 
 fn running_low_on_memory() -> bool {
